95452a59bc4b9be4d0ff25d06a3c1e124b597869
GowthamanLL <140971610+GowthamanLL@users.noreply.github.com>
2023-09-17 05:46:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,6 @@ def home():
     st.image(imageha)
     st.write('')
     st.write("There are a set of 200 target words were spoken in the carrier phrase 'Say the word _'' by two actresses (aged 26 and 64 years) and recordings were made of the set portraying each of seven emotions (anger, disgust, fear, happiness, pleasant surprise, sadness, and neutral). There are 2800 data points (audio files) in total.")
-    st.write('')
-    st.write("The dataset is organised such that each of the two female actor and their emotions are contain within its own folder. And within that, all 200 target words audio file can be found. The format of the audio file is a WAV format")
-    col1, col2,col3 = st.columns(3)
-
-    
-    col1.header(" Happy","Sad","Disguest","Angry","Fear","Surprise")
-    
 # Define the prediction function
 def predict_emotion(audio_data, sample_rate):
     predictions = model.predict(audio_data)
